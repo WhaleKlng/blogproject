1599cd381724e42107e9928cc9fb9a969b2296ab
xuhaopeng1224 <814350491@qq.com>
2019-10-16 17:55:43 +0800

    初步完成的Django博客 10/16

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,get_object_or_404
+import markdown,re
+from .models import Post
+from markdown.extensions.toc import TocExtension
+from django.utils.text import slugify
+
+# Create your views here.
+def index(request):
+    post_list = Post.objects.all()#.order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
+    ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
+
+    return render(request, 'blog/detail.html', context={'post': post})
