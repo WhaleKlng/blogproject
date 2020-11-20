bafa759aa090a760537c139ffe8f5d463c73db94
WhaleKlng <814350491@qq.com>
2020-11-20 14:19:01 +0800

    1120 初始化项目

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,7 +1,7 @@
-"""blogproject URL Configuration
+"""blogproject1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,17 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from blog.views import PostViewSet
-
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('', include('comments.urls')),
-    path("api/", include(router.urls)),
-    path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for blogproject project.
+WSGI config for blogproject1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
